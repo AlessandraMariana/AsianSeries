5aaf23a929b4811d3020e12ad2d3d2d6a1cc214d
Alessandra <ale.souzza12@gmail.com>
2021-10-17 21:17:05 -0300

    primeiro commit

---
+++ Classes/AsianSeriesRepositorio.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+{
+     
+}
+
+
+
+namespace Asian.Series.Classes
+{
+    public class AsianSeriesRepositorio
+    {
+        static AsianSeriesRepositorio repositorio = new AsianSeriesRepositorio();
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie serie);
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = serie;
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie.RemoveAt(id);
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie serie);
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(serie);
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public Serie RetornaPorId(int id);
+        public int ProximoId()
+        {
+            return listaSerie[id];
+            return listaSerie.Count;
+        }
+
+        public int ProximoId();
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie.Count;
+            return listaSerie[id];
+        }
+
+    }
+}
---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,9 @@
+namespace Asian.Series.Classes
+{
+    public abstract class EntidadeBase
+    {
+
+      public int Id {get;protected set;}
+
+    }
+}
